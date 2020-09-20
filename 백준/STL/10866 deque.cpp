1c148bf6a28c15745821138ef8d2fe501a56e902
yujeong <yji0806@naver.com>
2020-09-20 15:48:45 +0900

    Create 10866 deque.cpp

---
+++ 백준/STL/10866 deque.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <list>
+#include <pair>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "hello world";
+	/*
+	list<pair<int, int>> a;
+	int n;
+	cin >> n;
+	cout << n;
+	/*
+	for (int i = 1;i < n+1;i++) {
+		int j;
+		cin >> j;
+		a.push_back({ i,j });
+	}
+	
+	auto it = a.begin();
+	while (n--) {
+		cout << it->first << ' ';
+		int tmp = it->second;
+		
+		//a.erase(it);
+	}*/
+}

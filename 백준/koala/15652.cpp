39906b6cae40cd475fbad999c5c54d926785cb40
yujeong <yji0806@naver.com>
2021-02-14 14:42:34 +0900

    N과 M (4)
    백트래킹

---
+++ 백준/koala/15652.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+int n, m;
+int ans[8];
+
+void go(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i = 0;i < m;i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = idx;i <= n;i++) {
+		ans[cnt] = i;
+		go(i, cnt + 1);
+	}
+	return;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	go(1, 0);
+
+	return 0;
+}

cf1679f054360b10387ff07def122d45f819a173
yujeong <yji0806@naver.com>
2021-02-17 21:28:16 +0900

    최대 힙
    priority queue

---
+++ 백준/koala/11279.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int> q;
+
+	for (int i = 0;i < n;i++){
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (q.empty()) cout << "0\n";
+			else {
+				cout << q.top() << "\n";
+				q.pop();
+			}
+		}
+		else {
+			q.push(x);
+		}
+	}
+
+	return 0;
+}

868b73dd1f714492936ac54cd7f1181ca541d558
yujeong <yji0806@naver.com>
2021-02-25 20:25:51 +0900

    아기상어
    BFS
    시뮬레이션

--- 백준/koala/16236.cpp
+++ 백준/koala/16236.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <tuple>
 #include <cstring>
 #include <algorithm>
 using namespace std;
@@ -19,7 +20,7 @@ bool check[21][21];
 int dist[21][21];
 
 queue<pair<int, int>> q;
-vector<pair<int, int>> v;
+vector<tuple<int, int,int>> v;
 
 int x_[4] = { 0,0,1,-1 };
 int y_[4] = { 1,-1,0,0 };
@@ -45,8 +46,8 @@ void bfs() { // 거리의 최솟값을 구하는 함수
 				q.push({ next_y,next_x });
 				check[next_y][next_x] = true;
 				dist[next_y][next_x] = dist[node_y][node_x] + 1;
-				if (arr[next_y][next_x] < _size && arr[next_y][next_x] !=0) {
-					v.push_back({ next_y,next_x });
+				if (arr[next_y][next_x] < _size && arr[next_y][next_x] != 0) {
+					v.push_back(make_tuple(dist[next_y][next_x], next_y,next_x ));
 				}
 			}
 		}
@@ -61,11 +62,6 @@ void eat(int y, int x) {
 	memset(dist, 0, sizeof(dist));
 	memset(check, false, sizeof(check));
 	v.clear();
-}
-
-bool cmp(pair<int,int> x, pair<int,int> y) {
-	if (x.first == y.first) return x.second > y.second;
-	return x.first > y.first;
 }
 
 int main() {
@@ -91,36 +87,16 @@ int main() {
 		// 더 이상 먹을 수 있는 물고기가 공간에 없다면 종료
 		if (v.empty()) break;
 
-		// 먹을 수 있는 물고기가 1마리면, 걔를 먹음 (빈칸/시간++)
-		if (v.size() == 1) {
+		// 먹을 수 있는 물고기가 1마리 이상이면, 걔를 먹음 (빈칸/시간++)
+		if (v.size() >= 1) {
 			cnt++;
 
-			t += dist[v[0].first][v[0].second];
+			sort(v.begin(), v.end());
 
-			eat(v[0].first, v[0].second);
+			t += dist[get<1>(v[0])][get<2>(v[0])];
 
-		}
+			eat(get<1>(v[0]),get<2>(v[0]));
 
-		// 먹을 수 있는 물고기가 여러마리면, 가장 위, 왼쪽 우선순위 거리 물고기 먹음
-		else if (v.size() > 1) {
-			cnt++;
-
-			int min_num = 100;
-			pair<int, int> temp;
-
-			sort(v.begin(), v.end(), cmp);
-
-			for (int i = 0;i < v.size();i++) {
-				min_num =  min(min_num, dist[v[i].first][v[i].second]);
-				if (dist[v[i].first][v[i].second] == min_num) {
-					temp = { v[i].first,v[i].second };
-				}
-			}
-
-			t += min_num;
-
-			eat(temp.first,temp.second);
-			
 		}
 
 		// 먹은 마리 수 == size 면, size++
